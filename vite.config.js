d313b46d6bcf046a92350040c75dd7ea0c086b87
MariiaZhk <maria8zhuk@gmail.com>
2024-01-19 15:26:26 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/car-renal",
+  base: "/car-rental",
   plugins: [react()],
 });
